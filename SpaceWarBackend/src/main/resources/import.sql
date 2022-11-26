c5e42a381a9018b4790b182259d3c72090b47ade
Yassir Curbelo Halti <yassirch2001ch@gmail.com>
2022-11-26 03:14:11 +0000

    added security role at all endpoint and fixe some bugs, added auto import role to ease start up server

---
+++ SpaceWarBackend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO `db_proyecto_final2`.`roles` (`id`, `name`) VALUES ('1', 'ROLE_USER');
+INSERT INTO `db_proyecto_final2`.`roles` (`id`, `name`) VALUES ('2', 'ROLE_ADMIN');
+INSERT INTO `db_proyecto_final2`.`roles` (`id`, `name`) VALUES ('3', 'ROLE_MODERATOR');
