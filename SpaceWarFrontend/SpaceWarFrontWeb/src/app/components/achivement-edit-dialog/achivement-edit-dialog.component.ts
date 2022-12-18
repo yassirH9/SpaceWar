59f086d193f2ad756d72e91f4293bb6b4d958509
Yassir Curbelo Halti <yassirch2001ch@gmail.com>
2022-12-18 04:37:03 +0000

    some bugs fixed and added error andle in frontedn with alerts

--- SpaceWarFrontend/SpaceWarFrontWeb/src/app/components/achivement-edit-dialog/achivement-edit-dialog.component.ts
+++ SpaceWarFrontend/SpaceWarFrontWeb/src/app/components/achivement-edit-dialog/achivement-edit-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, PatternValidator } from '@angular/forms';
 import { EndpointServiceService } from 'src/app/service/endpoint-service.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatDialogRef } from '@angular/material/dialog';
@@ -15,83 +15,123 @@ import Swal from 'sweetalert2';
   styleUrls: ['./achivement-edit-dialog.component.scss']
 })
 export class AchivementEditDialogComponent {
-//input data
-id: any;
-plid: any;
-achid:any;
+  //input data
+  id: any;
+  plid: any;
+  achid: any;
 
-fulleditable: boolean;
-//form group
-editForm = new FormGroup({
-  id: new FormControl(''),
-  plid: new FormControl(''),
-  achid: new FormControl(''),
-})
+  fulleditable: boolean;
+  //form group
+  editForm = new FormGroup({
+    id: new FormControl(''),
+    plid: new FormControl(''),
+    achid: new FormControl(''),
+  })
 
-constructor(
-  public dialogRef: MatDialogRef<AchivementEditDialogComponent>,
-  private endpoint: EndpointServiceService,
-  @Inject(MAT_DIALOG_DATA) data: any,
-) {
-  //injet data
-  this.achid = data.achid;
+  constructor(
+    public dialogRef: MatDialogRef<AchivementEditDialogComponent>,
+    private endpoint: EndpointServiceService,
+    @Inject(MAT_DIALOG_DATA) data: any,
+  ) {
+    //injet data
+    this.achid = data.achid;
 
-  this.id = data.id;
-  this.plid = data.plid;
+    this.id = data.id;
+    this.plid = data.plid;
 
-  this.fulleditable = data.fullEditable;
-}
-ngOnInit() {
-  //añadir valores injectados al formulario de edicion
-  this.editForm.setValue({
-    id: this.id,
-    plid: this.plid,
-    achid: this.achid,
-  });
-
-  console.log(this.fulleditable);
-  if (this.fulleditable === true) {
-    document.getElementById('id')!.removeAttribute("readonly");
-    //document.getElementById('name')!.removeAttribute("readonly");
+    this.fulleditable = data.fullEditable;
   }
-}
-onNoClick() {
-  //cerrar formulario en caso de pulsar cancelar
-  this.dialogRef.close();
-}
-send() {
-  this.id = this.editForm.get("id")!.value;
-  this.plid = this.editForm.get("plid")!.value;
-  this.achid = this.editForm.get("achid")!.value;
+  ngOnInit() {
+    //añadir valores injectados al formulario de edicion
+    this.editForm.setValue({
+      id: this.id,
+      plid: this.plid,
+      achid: this.achid,
+    });
 
-  const user: ILoginResponse = {
-    plid: this.plid,
-    nickname: "",
-    mail:"",
-    accessToken:"",
-    rol:"",
-    pswd:"",      
+    console.log(this.fulleditable);
+    if (this.fulleditable === true) {
+      document.getElementById('id')!.removeAttribute("readonly");
+      //document.getElementById('name')!.removeAttribute("readonly");
+    }
   }
-  const masterachivement: MasterAchivement ={
-    id:this.id,
-    name:"",
-    description:"",
+  onNoClick() {
+    //cerrar formulario en caso de pulsar cancelar
+    this.dialogRef.close();
   }
-  const achivement: Achivement = {
-   userm:user,
-   masterAchivement: masterachivement,
-   id:this.achid,
-  }
+  send() {
+    this.id = this.editForm.get("id")!.value;
+    this.plid = this.editForm.get("plid")!.value;
+    this.achid = this.editForm.get("achid")!.value;
 
-  this.endpoint.putAchivement(achivement,this.achid).subscribe((data) => {
+    const user: ILoginResponse = {
+      plid: this.plid,
+      nickname: "",
+      mail: "",
+      accessToken: "",
+      rol: "",
+      pswd: "",
+    }
+    const masterachivement: MasterAchivement = {
+      id: this.id,
+      name: "",
+      description: "",
+    }
+    const achivement: Achivement = {
+      userm: user,
+      masterAchivement: masterachivement,
+      id: this.achid,
+    }
 
-  },(error)=>{
-    Swal.fire(
-      'Unexpected error',
-      'It is due to some problem with the server, please try again later.',
-      'warning'
-    )
-  });
-  this.dialogRef.close();
-}
+    Swal.fire({
+      title: 'Are you sure want to edit this achivement',
+      text: '',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, save it!',
+      confirmButtonColor: 'red',
+      cancelButtonText: 'No, keep it',
+      cancelButtonColor: 'black',
+    }).then((result) => {
+      if (result.value) {
+        this.endpoint.putAchivement(achivement, this.achid).subscribe((data) => {
+          this.dialogRef.close();
+        }, (error_) => {
+          if (error_.status == 504) {
+            Swal.fire(
+              'Please try again later',
+              'We are currently experiencing unexpected problems with the server.',
+              'warning'
+            )
+          }
+          if (error_.status == 500) {
+            Swal.fire(
+              'Please try again later',
+              'Server validation error',
+              'warning'
+            )
+          }
+          if (error_.status == 401) {
+            Swal.fire(
+              'Unauthorized',
+              'please log in.',
+              'warning'
+            )
+          }
+        });
+      }
+    })
+
+    //-------------------------------------------
+    //   this.endpoint.putAchivement(achivement,this.achid).subscribe((data) => {
+
+    //   },(error)=>{
+    //     Swal.fire(
+    //       'Unexpected error',
+    //       'It is due to some problem with the server, please try again later.',
+    //       'warning'
+    //     )
+    //   });
+    //   this.dialogRef.close();
+  }
 }
